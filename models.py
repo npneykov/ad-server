60144b57505da1484e69d7dce59c5ae02eee0f1d
Nikola Neykov <nikola.neykov@gmail.com>
2025-08-20 21:21:05 +0300

    add models file, modified pyproject

---
+++ models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from sqlmodel import Field, Relationship, SQLModel
+
+
+class Zone(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    width: int
+    height: int
+    ads: list['Ad'] = Relationship(back_populates='zone')
+
+
+class Ad(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    zone_id: int = Field(foreign_key='zone.id')
+    html: str
+    url: str
+    weight: int = 1
+    zone: Zone = Relationship(back_populates='ads')
+
+
+class Impression(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    ad_id: int = Field(foreign_key='ad.id')
+    timestamp: datetime = Field(default_factory=datetime.utcnow)
